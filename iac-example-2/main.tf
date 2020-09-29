701fbe190273ed5350cc00f0ffc42fd89d18d577
gargpriyank <priyank.garg@ibm.com>
2020-09-28 22:38:58 -0500

    Updated api key var

--- iac-example-2/main.tf
+++ iac-example-2/main.tf
@@ -1,10 +1,5 @@
-variable "iaas_classic_username" {
-}
-variable "iaas_classic_api_key" {
-}
+variable "ibmcloud_api_key" {}
 
 provider "ibm" {
-  region                = var.region
-  iaas_classic_username = var.iaas_classic_username
-  iaas_classic_api_key  = var.iaas_classic_api_key
+  ibmcloud_api_key = var.ibmcloud_api_key
 }
