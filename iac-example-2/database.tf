d205ab8ab53f4bc8fb6edfdb43137db50a9fe122
gargpriyank <priyank.garg@ibm.com>
2020-11-04 10:09:50 -0600

    Updated database instance

--- iac-example-2/database.tf
+++ iac-example-2/database.tf
@@ -1,4 +1,5 @@
 resource "ibm_resource_instance" "iac_app_db_instance" {
+  count             = var.enable_db_service ? 1 : 0
   name              = var.db_name
   service           = var.db_service_name
   plan              = var.db_plan
